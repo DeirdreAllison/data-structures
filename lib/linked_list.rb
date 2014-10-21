bcb54199b3e34d875ed637d7a71544b1c33d4197
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2014-10-20 18:26:00 -0700

    implement stack

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,18 +1,17 @@
 class Node
   Node = Struct.new(:val, :nn)
-    ll= Node.new(‘A’, l)
-    c = ll
-#to print
-    while c do
-      puts c.val
-      c = c.nn
-    end
+  ll = Node.new(A, l)
+  c = ll
+  # to print
+  while c
+    puts c.val
+    c = c.nn
+  end
 
   class List
     attr_accessor :head, :count
     def add(node)
-      count += 1
-      node.nn = @head
+      node.nn = head
       @head = node
     end
   end
