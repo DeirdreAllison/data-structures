1f5bc1f258947a7dec4d10bc554d10f765acccb1
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2014-10-20 18:20:46 -0700

    start singly linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,19 +1,37 @@
-class Node
-  Node = Struct.new(:val, :nn)
-    ll= Node.new(‘A’, l)
-    c = ll
-#to print
-    while c do
-      puts c.val
-      c = c.nn
-    end
+Node = Struct.new(:val, :nn)
 
   class List
     attr_accessor :head, :count
-    def add(node)
+    def insert(node)
       count += 1
       node.nn = @head
       @head = node
     end
+
+    def size
+      i = 0
+      n = head
+      while n
+        i += 1
+        n = n.nn
+      end
+      i
+    end
+
+    def search(val)
+    end
+
+    def remove(node)
+      prev = nil
+      n = head
+      while n
+        if n == node
+          prev ? prev.nn = node.nn : @head = head.nn
+          node.nn = nil
+          return node.val
+        end
+        prev = n
+        n = n.nn
+      end
+    end
   end
-end
