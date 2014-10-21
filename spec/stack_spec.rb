ac9bfac5f31b1a09cf95fc146efbb9a1b665f71f
DEIRDRE ALLISON <DEIRDRE9@GMAIL.com>
2014-10-20 20:04:27 -0700

    fix tests to pass

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -3,20 +3,20 @@ require 'stack'
 require 'linked_list'
 
 describe Stack do
-  before { s = Stack.new }
+  before { @s = Stack.new }
 
   it 'should insert element' do
-    s.size.must_equal nil
-    s.push(3)
-    s.size.must_equal 1
+    @s.size.must_equal 0
+    @s.push(3)
+    @s.size.must_equal 1
   end
 
   it 'should remove element' do
-    s.push(1)
-    s.push(5)
-    s.size.must_equal 2
-    s.pop
-    s.size.must_equal 1
+    @s.push(1)
+    @s.push(5)
+    @s.size.must_equal 2
+    @s.pop
+    @s.size.must_equal 1
   end
 
 end
